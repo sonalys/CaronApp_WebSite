c3313c347e1a7b5da26bef2c5b5038e02022a211
Alysson Ribeiro <alraicon@gmail.com>
2017-09-28 12:05:13 -0300

    Upload dos Arquivos

---
+++ index.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+?>
+<html> <!-- HTML TAG -->
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" /> <!-- External References -->
+<Script href="background.js" type="text/javascript"></script>
+ <!-- <div class="div_blur"></div>Blurry Background 
+<div class="div_overlay"></div>-->
+ <!-- Content Start -->
+<div class="content">
+	<div class="menu_lateral">
+		<div class="logo"></div>
+		<!--<details><summary>Login</summary>
+		<div class="login_panel"></div>
+		</details>-->
+		<ul class="lista_lateral">
+			<li><a href="index.php" ondragstart="return false;">Página Inicial</a></li>
+			<!--<li><a href="index.php" ondragstart="return false;">Perfil</a></li>-->
+			<li><a href="index.php" ondragstart="return false;">Buscar Carona</a></li>
+			<li><a href="index.php" ondragstart="return false;">Registrar Carona</a></li>
+		</ul>
+	</div>
+</div>
+<!--<div class="home_background">
+</div>-->
+<div class="menu_superior">
+	<div class="profile">
+		<a class="user_name dropdown_profile">Alysson Ribeiro</a>
+		<img class="user_icon dropdown_profile" src='https://i.pinimg.com/736x/32/c8/5c/32c85c66e56c16729b04de8e76051d04--anime-boys-anime-manga.jpg'>
+		<div class="login_panel"></div>
+	</div>
+</div>
+</html>
